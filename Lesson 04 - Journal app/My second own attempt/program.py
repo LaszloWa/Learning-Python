4fd1cffed21d9ce6936b4ab6de48e8bc3f4df459
PazuzuTriesCode <44098779+PazuzuTriesCode@users.noreply.github.com>
2018-11-11 00:16:32 +0100

    Journal app - "My second own attempt" - Deletion feature implemented
    Implemented the feature to delete individual entries. In order to implement that feature, I had to learn how to handle exceptions ("try:" and "exception:") and how to remove indivudal list items. It took me quite a while to implement the feature as I had some issues mapping the user input to the correct entry. This was mainly because the entries shown to the user were listed in the opposite order as they were actually in the stored list, and started counting at 1 rather than 0. But I managed in the end :)

--- Lesson 04 - Journal app/My second own attempt/program.py
+++ Lesson 04 - Journal app/My second own attempt/program.py
@@ -20,13 +20,15 @@ def run_journal():
 
 
     while cmd != "x" and cmd:
-        cmd = input("[A]dd entry, [S]how entries, E[x]it: ")
+        cmd = input("[A]dd entry, [S]how entries, [D]elete entry, E[x]it: ")
         cmd = cmd.lower().strip()
 
         if cmd == "a":
             add_entry(journal_data)
         elif cmd == "s":
             show_entry(journal_data)
+        elif cmd== "d":
+            delete_entry(journal_data)
         elif cmd != "x" and cmd:
             print("Sorry, I don't understand what '{}' means.".format(cmd))
 
@@ -41,8 +43,51 @@ def add_entry(data):
 
 
 def show_entry(data):
+    data = reversed(data)
     for ind, items in enumerate(data):
         print("[{}] {}".format(ind + 1, items))
+
+
+# Added this function in order to delete entries from the list.
+def delete_entry(data):
+    # Before asking which entry should be deleted, the list of entries is shown to the user.
+    show_entry(data)
+    # The user is asked which entry they would like to delete.
+    print("Which entry would you like to delete?")
+
+    # The user enters their choice as a string (to avoid ValueErrors if the user just hits enter).
+    # The user is asked to enter a value between 1 and the total length of the list.
+    user_choice = input("Please enter a value between 1 and {}, or 0 to abort: ".format(len(data)))
+
+    # This I only learned during this attempt. Because the user input is a string, and it needs to be an
+    # integer to use it further down, this checks if the user input can be turned into an integer,
+    # but if it can't be turned into an integer, the exception is handled in a way controlled by the code
+    # rather than the program just throwing out an error code and ending the program.
+    try:
+        user_choice_int = 0
+        user_choice_int = int(user_choice)
+    except ValueError:
+        print("Please enter either a value between 1 and {} or 0.".format(len(data)))
+
+    # Once the user input is turned into an integer, the user input is subtracted from the length of the list.
+    # This is done because we are showing the list in reversed form, which means that in the list shown to the user
+    # Item 1 is actually the LAST item in the list, so in a list with len = 4, item 1 in the list shown to the user
+    # would actually be item 3 (remember that lists start at 0) in the list journal_data. Confusing, right?
+    user_choice_int = len(data) - user_choice_int
+
+    # Conditionals for deciding what to do with the user's input.
+    if user_choice_int >= 0 and user_choice_int <= len(data):
+        for ind, items in enumerate(data):
+            if ind == user_choice_int:
+                data.pop(ind)
+                print("Deleting '{}'.".format(items))
+                break
+    elif user_choice_int == 0:
+        pass
+    else:
+        pass
+
+    return data
 
 
 main()
