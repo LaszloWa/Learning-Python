7d81b673f2dd6b7ec4235f314289946a2aa65806
PazuzuTriesCode <44098779+PazuzuTriesCode@users.noreply.github.com>
2018-11-19 22:02:43 +0100

    Weather app - Initial instructor led attempt
    Following the video instructions bit by bit to understand the process of making this app.

---
+++ Lesson 05 - Weather App/program.py
@@ -0,0 +1,62 @@
+import requests
+import bs4
+
+
+def main():
+    print_header()
+    zip_code = input("For what zip-code would you like the weather forecast? ")
+
+    html = get_html_from_web(zip_code)
+    get_weather_from_html(html)
+    #print_weather
+
+
+def print_header():
+    print("---------------------------------")
+    print("          Weather App")
+    print("---------------------------------")
+    print()
+
+
+def get_html_from_web(zip_code):
+    url = "https://www.wunderground.com/weather-forecast/{}".format(zip_code)
+
+    response = requests.get(url)
+
+    return response.text
+
+
+def get_weather_from_html(html):
+    # cityCSS = ".region-content-header h1"
+    # weatherScaleCSS = ".wu-unit-temperature .wu-label"
+    # weatherTemCSS = ".wu-unit-temperature .wu-value"
+    # weatherConditionCSS = ".condition-icon"
+
+    soup = bs4.BeautifulSoup(html, "html.parser")
+    loc = soup.find(id="inner-content").find("h1").get_text()
+    temp = soup.find(id="inner-content").find(class_="wu-value wu-value-to").get_text()
+    unit = soup.find(id="inner-content").find(class_="wu-label").get_text()
+    condition = soup.find(id="inner-content").find(class_="condition-icon small-6 medium-12 columns").get_text()
+
+    loc = clean_up(loc)
+    loc = loc_clean_up(loc)
+    temp = clean_up(temp)
+    unit = clean_up(unit)
+    condition = clean_up(condition)
+
+
+def loc_clean_up(text : str):
+    parts = text.split("\n")
+    return parts[0].strip()
+
+
+def clean_up(text):
+    if not text:
+        return text
+
+    else:
+        text.strip()
+    return text
+
+
+main()
