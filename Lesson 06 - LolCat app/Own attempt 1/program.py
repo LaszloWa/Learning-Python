5330c2ee57b5c13673d56dce5a1dad63e552ad85
PazuzuTriesCode <44098779+PazuzuTriesCode@users.noreply.github.com>
2018-11-27 13:32:20 +0100

    LolCat app - Own attempt 1
    This is my first attempt to code the lolcat app on my own. Still needed to look up quite a few things, but overall it went well enough.

---
+++ Lesson 06 - LolCat app/Own attempt 1/program.py
@@ -0,0 +1,54 @@
+import os
+import platform
+import subprocess
+
+import cat_service
+
+
+def main():
+    print_header()
+    folder = get_or_create_output_folder()
+    download_cats(folder)
+    display_cats(folder)
+
+
+def print_header():
+    print("-------------------------------------")
+    print("            Cat Factory")
+    print("-------------------------------------")
+    print()
+
+
+def get_or_create_output_folder():
+    base_folder = os.path.dirname(__file__)
+    folder = "cat_pictures"
+    full_path = os.path.join(base_folder, folder)
+
+    if not os.path.exists(full_path) or not os.path.isdir(full_path):
+        print("Creating new directory at {}.".format(full_path))
+        os.mkdir(full_path)
+
+    return full_path
+
+
+def download_cats(folder):
+    cat_count = 8
+    for i in range(1, cat_count + 1):
+        name = "lolcat {}".format(i)
+        print("Downloading {}.".format(name))
+        cat_service.get_cats(folder, name)
+
+
+def display_cats(folder):
+    if platform.system() == "Darwin":
+        subprocess.call("open", folder)
+    elif platform.system() == "Windows":
+        subprocess.call("explorer", folder)
+    elif platform.system() == "Linux":
+        subprovess.call("xdg-open", folder)
+    else:
+        print("Sorry, but your OS {} is not supported.".format(platform.system()))
+
+
+if __name__ == "__main__":
+    main()
